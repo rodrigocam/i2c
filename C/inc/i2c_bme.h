833f32205549f98784bb6c39e7fda08b72d81745
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-09-21 15:12:07 -0300

    Add python code

--- C/inc/i2c_bme.h
+++ C/inc/i2c_bme.h
@@ -14,6 +14,6 @@ void delay_us(uint32_t period, void *intf_ptr);
 
 int8_t i2c_write(uint8_t reg_addr, const uint8_t *data, uint32_t len, void *intf_ptr);
 
-void print_sensor_data(struct bme280_data *comp_data);
+void print_sensor_data(struct bme280_data *compilete_data);
 
 int8_t stream_sensor_data_forced_mode(struct bme280_dev *dev);
---
+++ python/src/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import smbus2
+import bme280
+
+import RPi_I2C_driver
+
+
+I2C_PORT = 1
+DEVICE_ADDR = 0x76
+
+BUS = smbus2.SMBus(I2C_PORT)
+CALIBRATION_PARAMS = bme280.load_calibration_params(BUS, DEVICE_ADDR)
+
+LCD = RPi_I2C_driver.lcd()
+LCD.lcd_clear()
+
+while True:
+    DATA = bme280.sample(BUS, DEVICE_ADDR, CALIBRATION_PARAMS)
+    LCD.lcd_display_string(f"T. {DATA.temperature:.2f}, H. {DATA.humidity:.2f}, P. {DATA.pressure:.2f}", 1) 
+    LCD.lcd_clear()
+    sleep(1)
