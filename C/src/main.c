dc0c25ee91600b360ed303494c68f2bc4dbe6fcd
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-09-21 14:25:28 -0300

    Refactor

--- C/src/main.c
+++ C/src/main.c
@@ -11,38 +11,41 @@
 #include <bme280.h>
 #include <i2c_bme.h>
 
+#define I2C_FILE "/dev/i2c-1"
 
 int main() {
-    struct bme280_dev dev;
+    struct bme280_dev device;
+    
     struct identifier id;
+    id.dev_addr = BME280_I2C_ADDR_PRIM;
 
     int8_t result = BME280_OK;
 
-    if ((id.fd = open("/dev/i2c-1", O_RDWR)) < 0) {
-	fprintf(stderr, "Failed to open the i2c bus %s\n", "/dev/i2c-1");
+    if ((id.fd = open(I2C_FILE, O_RDWR)) < 0) {
+	fprintf(stderr, "Failed to open the i2c bus %s\n", I2C_FILE);
 	exit(1);
-}
-    if(ioctl(id.fd, I2C_SLAVE, BME280_I2C_ADDR_PRIM) < 0) {
+    }
+    
+    if(ioctl(id.fd, I2C_SLAVE, id.dev_addr) < 0) {
         fprintf(stderr, "Failed to acquire bus access\n");
         exit(1);
     }
 
-    id.dev_addr = BME280_I2C_ADDR_PRIM;
+    device.intf = BME280_I2C_INTF;
+    device.read = i2c_read;
+    device.write = i2c_write;
+    device.delay_us = delay_us;
+    device.intf_ptr = &id;
 
-    dev.intf = BME280_I2C_INTF;
-    dev.read = i2c_read;
-    dev.write = i2c_write;
-    dev.delay_us = delay_us;
-
-    dev.intf_ptr = &id;
-
-    result = bme280_init(&dev);
+    result = bme280_init(&device);
+    
     if(result != BME280_OK) {
         fprintf(stderr, "Failed to initialize the device (code %+d).\n", result);
         exit(1);
     }
 
-    result = stream_sensor_data_forced_mode(&dev);
+    result = stream_sensor_data_forced_mode(&device);
+
     if (result != BME280_OK) {
         fprintf(stderr, "Failed to stream sensor data (code %+d).\n", result);
         exit(1);
